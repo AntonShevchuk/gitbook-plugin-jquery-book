8b62df1eccb42278731f4096d7cbdb30e8f7ae5e
Anton Shevchuk <AntonShevchuk@gmail.com>
2018-02-21 18:36:20 +0200

    Added jQuery 3.3.1

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  book: {
+    assets: './assets',
+    js: [
+      'jquery.js',
+      'code.js'
+    ],
+    css: [
+      'styles.css'
+    ]
+  },
+  // Map of hooks
+  hooks: {},
+
+  // Map of new blocks
+  blocks: {},
+
+  // Map of new filters
+  filters: {}
+};
