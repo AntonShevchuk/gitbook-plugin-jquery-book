9f9924fd705e0054533612b0f201a8b27a0a9841
Anton Shevchuk <AntonShevchuk@gmail.com>
2018-02-22 18:47:40 +0200

    Added type `append-style`

--- assets/code.js
+++ assets/code.js
@@ -1,3 +1,21 @@
 /**
  * @author Anton Shevchuk
  */
+/* global define,require*/
+require(['jquery'], function ($) {
+  'use strict';
+  $(function () {
+    $('a.jqbook[data-type="append-style"]').click(appendStyle);
+  });
+
+  function appendStyle() {
+    let $element = $(this);
+    let $target = $($element.attr('href'));
+
+    $target.contents()
+      .find('head')
+      .append('<style>' + $element.text() + '</style>');
+
+    return false;
+  }
+});
