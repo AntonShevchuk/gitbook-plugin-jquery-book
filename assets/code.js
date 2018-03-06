7c77aeb78b42e9bc9543fda528c66064cb2d3c94
Anton Shevchuk <AntonShevchuk@gmail.com>
2018-03-06 10:06:35 +0200

    Added highlight Added pin for iframe

--- assets/code.js
+++ assets/code.js
@@ -6,6 +6,8 @@ require(['jquery'], function ($) {
   'use strict';
   $(function () {
     $(document).on('click', 'a.jqbook[data-type="append-style"]', appendStyle);
+    $(document).on('click', 'a.jqbook[data-type="highlight"]', highlight);
+    $(document).on('click', 'button', switchSticky);
   });
 
   function appendStyle(event) {
@@ -18,6 +20,25 @@ require(['jquery'], function ($) {
       .find('head')
       .append('<style>' + $element.text() + '</style>');
 
+    return false;
+  }
+
+  function highlight(event) {
+    event.preventDefault();
+
+    let $element = $(this);
+    let $target = $($element.data('target'));
+
+    $target.contents()
+      .find('head')
+      .append('<script>' + $element.text() + '.effect("highlight", 800)</script>');
+
+    return false;
+  }
+
+  function switchSticky(event) {
+    event.preventDefault();
+    $(this).parent('div.jqbook').toggleClass('sticky');
     return false;
   }
 });
