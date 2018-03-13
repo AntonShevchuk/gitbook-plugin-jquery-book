cee60609fb1378dba00fb23d70700d5b749a8f3b
Anton Shevchuk <AntonShevchuk@gmail.com>
2018-03-13 12:52:23 +0200

    Added append script Added run script button

--- assets/code.js
+++ assets/code.js
@@ -6,8 +6,11 @@ require(['jquery'], function ($) {
   'use strict';
   $(function () {
     $(document).on('click', 'a.jqbook[data-type="append-style"]', appendStyle);
+    $(document).on('click', 'a.jqbook[data-type="append-script"]', appendScript);
     $(document).on('click', 'a.jqbook[data-type="highlight"]', highlight);
-    $(document).on('click', 'button', switchSticky);
+
+    $(document).on('click', 'button.jqbook.sticky', switchSticky);
+    $(document).on('click', 'button.jqbook.run', runScript);
   });
 
   function appendStyle(event) {
@@ -19,6 +22,32 @@ require(['jquery'], function ($) {
     $target.contents()
       .find('head')
       .append('<style>' + $element.text() + '</style>');
+
+    return false;
+  }
+
+  function appendScript(event) {
+    event.preventDefault();
+
+    let $element = $(this);
+    let $target = $($element.data('target'));
+
+    $target.contents()
+      .find('head')
+      .append('<script>' + $element.text() + '</script>');
+
+    return false;
+  }
+
+  function runScript(event) {
+    event.preventDefault();
+
+    let $element = $(this);
+    let $target = $($element.data('target'));
+
+    $target.contents()
+      .find('head')
+      .append('<script>' + $element.next().text() + '</script>');
 
     return false;
   }
